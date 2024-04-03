51c61302c6a0fcda9234f9fcfba3f8142e6d1696
ritec03 <64804506+ritec03@users.noreply.github.com>
2024-04-03 16:28:59 -0700

    refactor database functions into a class and update tests

--- database.py
+++ database.py
@@ -28,181 +28,190 @@ class ValueDoesNotExistInDB(LookupError):
     """
     pass
 
-def connect_to_db(db_path: str) -> Connection:
-    """
-    Connect to database specified by path and enable
-    foreign key checks.
+class DatabaseManager:
+    def __init__(self, db_path: str):
+        """
+        Initialize the database manager and establish a connection to the database.
+        """
+        self.connection = self.connect_to_db(db_path)
 
-    Args:
-        db_path: str - path to database
-    """
-    conn = sqlite3.connect(db_path)
-    conn.execute("PRAGMA foreign_keys = ON") # add checks on foreign keys
-    return conn
+    def connect_to_db(self, db_path: str) -> Connection:
+        """
+        Connect to database specified by path and enable
+        foreign key checks.
 
-def create_db(conn: Connection) -> None:
-    # Create a cursor object to execute SQL commands
-    cur = conn.cursor()
+        Args:
+            db_path: str - path to database
+        """
+        conn = sqlite3.connect(db_path)
+        conn.execute("PRAGMA foreign_keys = ON") # add checks on foreign keys
+        return conn
 
-    # Read the SQL file
-    with open(SCHEMA_PATH, 'r') as file:
-        sql_script = file.read()
+    def create_db(self) -> None:
+        # Create a cursor object to execute SQL commands
+        cur = self.connection.cursor()
 
-    # Execute the SQL script
-    cur.executescript(sql_script)
+        # Read the SQL file
+        with open(SCHEMA_PATH, 'r') as file:
+            sql_script = file.read()
 
-    # Commit the transaction (save changes)
-    conn.commit()
+        # Execute the SQL script
+        cur.executescript(sql_script)
 
-    # Close the cursor and connection
-    cur.close()
+        # Commit the transaction (save changes)
+        self.connection.commit()
 
-def add_words_to_db(conn: Connection, word_list: List[Tuple[str, str, int]]) -> None:
-    """
-    Insert tuples of (word, part-of-speech, frequency) into words table.
-    If a combination of (word, pos) already exists in the database,
-    only the freq count is updated.
-
-    Args:
-        conn (Connection): SQLite database connection.
-        word_list (List[Tuple[str, str, int]]): list of tuples of (word, pos, freq),
-            eg. [("Schule", "NOUN", 234), ...]
-    """
-    cur = conn.cursor()
-    sql_insert = "INSERT INTO words (word, pos, freq) VALUES (?, ?, ?) ON CONFLICT(word, pos) DO UPDATE SET freq = excluded.freq"
-    cur.executemany(sql_insert, word_list)
-    conn.commit()
-    cur.close()
-
-def query_first_ten_entries(conn: Connection, table_name: str) -> None:
-    """
-    Print first ten entries of table with table_name if table exists.
-    If table does not exist, print "table does not exist".
-
-    Args:
-        conn (Connection): SQLite database connection.
-        table_name (str): Name of the table to query.
-    """
-    cur = conn.cursor()
-    try:
-        cur.execute(f"SELECT * FROM {table_name} LIMIT 10")
-        rows = cur.fetchall()
-        for row in rows:
-            print(row)
-    except sqlite3.OperationalError:
-        print(f"Table '{table_name}' does not exist.")
-    finally:
+        # Close the cursor and connection
         cur.close()
 
-def insert_user(conn: Connection, user_name: str) -> None:
-    """
-    Insert a new user into the users table.
+    def add_words_to_db(self, word_list: List[Tuple[str, str, int]]) -> None:
+        """
+        Insert tuples of (word, part-of-speech, frequency) into words table.
+        If a combination of (word, pos) already exists in the database,
+        only the freq count is updated.
 
-    Args:
-        conn (Connection): SQLite database connection.
-        user_name (str): Username of the user to insert.
-
-    Raises:
-        ValueError: If the user already exists in the database.
-        ValueError: If the user name is not a string or is longer than MAX_USER_NAME_LENGTH
-    """
-    if type(user_name) is not str or len(user_name) > MAX_USER_NAME_LENGTH:
-        raise ValueError("Username is not a string or too long.")
-    cur = conn.cursor()
-    try:
-        cur.execute("INSERT INTO users (user_name) VALUES (?)", (user_name,))
-        conn.commit()
-    except conn.IntegrityError:
-        raise ValueError(f"User '{user_name}' already exists in the database.")
-    finally:
+        Args:
+            word_list (List[Tuple[str, str, int]]): list of tuples of (word, pos, freq),
+                eg. [("Schule", "NOUN", 234), ...]
+        """
+        cur = self.connection.cursor()
+        sql_insert = "INSERT INTO words (word, pos, freq) VALUES (?, ?, ?) ON CONFLICT(word, pos) DO UPDATE SET freq = excluded.freq"
+        cur.executemany(sql_insert, word_list)
+        self.connection.commit()
         cur.close()
 
-def remove_user(conn: Connection, user_id: int) -> None:
-    """
-    Remove user with user_id from users table
-    and remove all associated rows in learning_data.
+    def query_first_ten_entries(self, table_name: str) -> None:
+        """
+        Print first ten entries of table with table_name if table exists.
+        If table does not exist, print "table does not exist".
 
-    Args:
-        conn (Connection): SQLite database connection.
-        user_id (int): ID of the user to remove.
-    
-    Raises:
-        ValueDoesNotExistInDB if the user with user_id does not exist
-    """
-    cur = conn.cursor()
-    try:
-        cur.execute("DELETE FROM users WHERE id=?", (user_id,))
-        if cur.rowcount == 0:
-            raise ValueDoesNotExistInDB(f"User with ID {user_id} does not exist.")
-        else:
-            cur.execute("DELETE FROM learning_data WHERE user_id=?", (user_id,))
-            conn.commit()
-            print(f"User with ID {user_id} removed successfully.")
-    finally:
-        cur.close()
+        Args:
+            table_name (str): Name of the table to query.
+        """
+        cur = self.connection.cursor()
+        try:
+            cur.execute(f"SELECT * FROM {table_name} LIMIT 10")
+            rows = cur.fetchall()
+            for row in rows:
+                print(row)
+        except sqlite3.OperationalError:
+            print(f"Table '{table_name}' does not exist.")
+        finally:
+            cur.close()
 
-def add_word_score(conn: Connection, user_id: int, word_id: int, score: int) -> None:
-    """
-    Add a row to learning_data for user with user_id
-    for word_id and score. Score should be between MIN_SCORE and MAX_SCORE.
+    def insert_user(self, user_name: str) -> None:
+        """
+        Insert a new user into the users table.
 
-    Args:
-        conn (Connection): SQLite database connection.
-        user_id (int): ID of the user.
-        word_id (int): ID of the word.
-        score (int): Score to add (MIN_SCORE and MAX_SCORE).
-    """
-    if not MIN_SCORE <= score <= MAX_SCORE:
-        raise ValueError(f"Score should be between {MIN_SCORE} and {MAX_SCORE}.")
-    
-    cur = conn.cursor()
-    try:
-        cur.execute("INSERT INTO learning_data (user_id, word_id, score) VALUES (?, ?, ?)", (user_id, word_id, score))
-        conn.commit()
-        print("Word score added successfully.")
-    except IntegrityError:
-        # Check if user_id or word_id does not exist
-        cur.execute("SELECT 1 FROM users WHERE id=?", (user_id,))
-        print(cur.fetchone())
-        if not cur.fetchone():
-            raise ValueDoesNotExistInDB(f"User with ID {user_id} does not exist.")
-        else:
-            cur.execute("SELECT 1 FROM words WHERE id=?", (word_id,))
+        Args:
+            user_name (str): Username of the user to insert.
+
+        Raises:
+            ValueError: If the user already exists in the database.
+            ValueError: If the user name is not a string or is longer than MAX_USER_NAME_LENGTH
+        """
+        if type(user_name) is not str or len(user_name) > MAX_USER_NAME_LENGTH:
+            raise ValueError("Username is not a string or too long.")
+        cur = self.connection.cursor()
+        try:
+            cur.execute("INSERT INTO users (user_name) VALUES (?)", (user_name,))
+            self.connection.commit()
+        except self.connection.IntegrityError:
+            raise ValueError(f"User '{user_name}' already exists in the database.")
+        finally:
+            cur.close()
+
+    def remove_user(self, user_id: int) -> None:
+        """
+        Remove user with user_id from users table
+        and remove all associated rows in learning_data.
+
+        Args:
+            user_id (int): ID of the user to remove.
+        
+        Raises:
+            ValueDoesNotExistInDB if the user with user_id does not exist
+        """
+        cur = self.connection.cursor()
+        try:
+            cur.execute("DELETE FROM users WHERE id=?", (user_id,))
+            if cur.rowcount == 0:
+                raise ValueDoesNotExistInDB(f"User with ID {user_id} does not exist.")
+            else:
+                cur.execute("DELETE FROM learning_data WHERE user_id=?", (user_id,))
+                self.connection.commit()
+                print(f"User with ID {user_id} removed successfully.")
+        finally:
+            cur.close()
+
+    def add_word_score(self, user_id: int, word_id: int, score: int) -> None:
+        """
+        Add a row to learning_data for user with user_id
+        for word_id and score. Score should be between MIN_SCORE and MAX_SCORE.
+
+        Args:
+            user_id (int): ID of the user.
+            word_id (int): ID of the word.
+            score (int): Score to add (MIN_SCORE and MAX_SCORE).
+        """
+        if not MIN_SCORE <= score <= MAX_SCORE:
+            raise ValueError(f"Score should be between {MIN_SCORE} and {MAX_SCORE}.")
+        
+        cur = self.connection.cursor()
+        try:
+            cur.execute("INSERT INTO learning_data (user_id, word_id, score) VALUES (?, ?, ?)", (user_id, word_id, score))
+            self.connection.commit()
+            print("Word score added successfully.")
+        except IntegrityError:
+            # Check if user_id or word_id does not exist
+            cur.execute("SELECT 1 FROM users WHERE id=?", (user_id,))
+            print(cur.fetchone())
             if not cur.fetchone():
-                raise ValueDoesNotExistInDB(f"Word with ID {word_id} does not exist.")
-    finally:
-        cur.close()
+                raise ValueDoesNotExistInDB(f"User with ID {user_id} does not exist.")
+            else:
+                cur.execute("SELECT 1 FROM words WHERE id=?", (word_id,))
+                if not cur.fetchone():
+                    raise ValueDoesNotExistInDB(f"Word with ID {word_id} does not exist.")
+        finally:
+            cur.close()
 
-def retrieve_user_scores(conn: Connection, user_id: int) -> List[Score]:
-    """
-    Retrieves word score data of a suer from the learning_data table
-    and returnes them as a list of Score.
-    """
-    raise NotImplementedError()
+    def retrieve_user_scores(self, user_id: int) -> List[Score]:
+        """
+        Retrieves word score data of a suer from the learning_data table
+        and returnes them as a list of Score.
+        """
+        raise NotImplementedError()
 
-def save_user_lesson_data(conn: Connection, user_id: int, lesson_data: List[Evaluation]) -> None:
-    """
+    def save_user_lesson_data(self, user_id: int, lesson_data: List[Evaluation]) -> None:
+        """
+        
+        """
+        raise NotImplementedError()
+
+    def update_user_scores(self, user_id: int, lesson_scores: List[Score]) -> None:
+        """
+        
+        """
+        raise NotImplementedError()
+
+    def fetch_tasks(self, criteria: List) -> List[Task]:
+        query = self.compose_query_from_criteria(criteria)
+        # Execute the query against the database
+        raise NotImplementedError()
+
+    def compose_query_from_criteria(self, criteria: List) -> str:
+        # This method should translate the criteria objects into a database query.
+        raise NotImplementedError()
     
-    """
-    raise NotImplementedError()
+    def close(self):
+        """
+        Closes the database connection.
+        """
+        if self.connection:
+            self.connection.close()
 
-def update_user_scores(conn: Connection, user_id: int, lesson_scores: List[Score]) -> None:
-    """
-    
-    """
-    raise NotImplementedError()
-
-def fetch_tasks(self, criteria: List) -> List[Task]:
-    query = self.compose_query_from_criteria(criteria)
-    # Execute the query against the database
-    raise NotImplementedError()
-
-def compose_query_from_criteria(self, criteria: List) -> str:
-    # This method should translate the criteria objects into a database query.
-    raise NotImplementedError()
-
-db_conn = connect_to_db(DATABASE_PATH)
-create_db(db_conn)
+db = DatabaseManager(DATABASE_PATH)
+db.create_db()
 
 word_freq_output_file_path = "word_freq.txt"
 word_freq_df_loaded = pd.read_csv(word_freq_output_file_path, sep="\t")
@@ -210,5 +219,5 @@ filtered_dataframe = word_freq_df_loaded[word_freq_df_loaded["count"] > 2]
 list_of_tuples: List[Tuple[str, str, int]] = list(filtered_dataframe.to_records(index=False))
 # convert numpy.int64 to Python integer
 list_of_tuples = [(word, pos, int(freq)) for (word, pos, freq) in list_of_tuples]
-add_words_to_db(db_conn, list_of_tuples)
-db_conn.close()
+db.add_words_to_db(list_of_tuples)
+db.close()
--- database_test.py
+++ database_test.py
@@ -1,6 +1,6 @@
 import unittest
 import sqlite3
-from database import create_db, insert_user, remove_user, add_word_score, add_words_to_db, ValueDoesNotExistInDB, MIN_SCORE, MAX_SCORE, MAX_USER_NAME_LENGTH, connect_to_db
+from database import MAX_SCORE, MAX_USER_NAME_LENGTH, MIN_SCORE, DatabaseManager, SCHEMA_PATH, ValueDoesNotExistInDB
 import os
 
 # Define a test database file path
@@ -8,18 +8,21 @@ TEST_DB_FILE = "test_database.db"
 
 class TestDatabaseFunctions(unittest.TestCase):
     def setUp(self):
-        # Connect to the test database and create tables
-        self.conn = connect_to_db(TEST_DB_FILE)
-        create_db(self.conn)
+        # Ensure the test database does not exist before starting each test
+        if os.path.exists(TEST_DB_FILE):
+            os.remove(TEST_DB_FILE)
+        self.db_manager = DatabaseManager(TEST_DB_FILE)
+        self.db_manager.create_db(SCHEMA_PATH)
 
     def tearDown(self):
         # Close the database connection and delete the test database file
-        self.conn.close()
-        os.remove(TEST_DB_FILE)
+        self.db_manager.close()
+        if os.path.exists(TEST_DB_FILE):
+            os.remove(TEST_DB_FILE)
 
     def test_insert_user(self):
         # Test inserting a new user
-        insert_user(self.conn, "test_user")
+        self.db_manager.insert_user(self.conn, "test_user")
         # Assert that the user is inserted successfully
         cur = self.conn.cursor()
         cur.execute("SELECT user_name FROM users WHERE user_name=?", ("test_user",))
@@ -29,14 +32,14 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def test_insert_duplicate_user(self):
         # Test inserting a user with the same user name (should fail)
-        insert_user(self.conn, "test_user")
+        self.db_manager.insert_user(self.conn, "test_user")
         with self.assertRaises(ValueError):
-            insert_user(self.conn, "test_user")  # Inserting the same user name should raise IntegrityError
+            self.db_manager.insert_user(self.conn, "test_user")  # Inserting the same user name should raise IntegrityError
 
     def test_remove_user_success(self):
         # Test removing an existing user
-        insert_user(self.conn, "test_user")
-        remove_user(self.conn, 1)  # Assuming the ID of the user just inserted is 1
+        self.db_manager.insert_user(self.conn, "test_user")
+        self.db_manager.remove_user(self.conn, 1)  # Assuming the ID of the user just inserted is 1
         # Assert that the user is removed successfully
         cur = self.conn.cursor()
         cur.execute("SELECT user_name FROM users WHERE id=?", (1,))
@@ -46,23 +49,23 @@ class TestDatabaseFunctions(unittest.TestCase):
     def test_remove_nonexistent_user(self):
         # Test removing a non-existent user
         with self.assertRaises(ValueDoesNotExistInDB):
-            remove_user(self.conn, 1)  # Attempting to remove a non-existent user should raise an exception
+            self.db_manager.remove_user(self.conn, 1)  # Attempting to remove a non-existent user should raise an exception
             
     def test_add_user_invalid_name_type(self):
         # Test adding a user with a user name that is not a string
         with self.assertRaises(ValueError):
-            insert_user(self.conn, 123)  # Inserting a non-string user name should raise a ValueError
+            self.db_manager.insert_user(self.conn, 123)  # Inserting a non-string user name should raise a ValueError
 
     def test_add_user_long_name(self):
         # Test adding a user with a user name that is too long
         long_username = "a" * (MAX_USER_NAME_LENGTH + 1)  # Create a user name longer than MAX_USER_NAME_LENGTH
         with self.assertRaises(ValueError):
-            insert_user(self.conn, long_username)  # Inserting a long user name should raise a ValueError
+            self.db_manager.insert_user(self.conn, long_username)  # Inserting a long user name should raise a ValueError
 
     def test_add_two_word_entries(self):
         # Test adding two word entries to the words table successfully
         word_list = [("cat", "NOUN", 10), ("dog", "NOUN", 5)]
-        add_words_to_db(self.conn, word_list)
+        self.db_manager.add_words_to_db(self.conn, word_list)
 
         # Assert that the entries are added successfully
         cur = self.conn.cursor()
@@ -80,9 +83,9 @@ class TestDatabaseFunctions(unittest.TestCase):
         OLD_FREQ = 10
         NEW_FREQ = 15
         word_list = [("cat", "NOUN", OLD_FREQ)]
-        add_words_to_db(self.conn, word_list)  # Add initial entry
+        self.db_manager.add_words_to_db(self.conn, word_list)  # Add initial entry
         word_list_update = [("cat", "NOUN", NEW_FREQ)]
-        add_words_to_db(self.conn, word_list_update)  # Update frequency
+        self.db_manager.add_words_to_db(self.conn, word_list_update)  # Update frequency
 
         # Assert that the frequency is updated
         cur = self.conn.cursor()
@@ -93,8 +96,8 @@ class TestDatabaseFunctions(unittest.TestCase):
     def test_add_existing_word_entry(self):
         # Test adding an existing word/pos entry to the words table
         word_list = [("cat", "NOUN", 10)]
-        add_words_to_db(self.conn, word_list)  # Add initial entry
-        add_words_to_db(self.conn, word_list)  # Attempt to add the same entry again
+        self.db_manager.add_words_to_db(self.conn, word_list)  # Add initial entry
+        self.db_manager.add_words_to_db(self.conn, word_list)  # Attempt to add the same entry again
 
         # Assert that the entry remains the same (nothing happens)
         cur = self.conn.cursor()
@@ -104,16 +107,16 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def test_add_word_score(self):
         # Test adding a word score
-        insert_user(self.conn, "test_user")
+        self.db_manager.insert_user(self.conn, "test_user")
         word_list = [("cat", "NOUN", 10)]
-        add_words_to_db(self.conn, word_list)
+        self.db_manager.add_words_to_db(self.conn, word_list)
         # Get the id of the word and the user
         cur = self.conn.cursor()
         cur.execute("SELECT id FROM words WHERE word=? AND pos=?", ("cat", "NOUN"))
         word_id = cur.fetchone()[0]
         cur.execute("SELECT id FROM users WHERE user_name=?", ("test_user",))
         user_id = cur.fetchone()[0]
-        add_word_score(self.conn, user_id, word_id, 8)  # Assuming user_id and word_id are both 1
+        self.db_manager.add_word_score(self.conn, user_id, word_id, 8)  # Assuming user_id and word_id are both 1
 
         # Assert that the word score is added successfully
         cur.execute("SELECT score FROM learning_data WHERE user_id=? AND word_id=?", (user_id, word_id))
@@ -122,30 +125,30 @@ class TestDatabaseFunctions(unittest.TestCase):
 
     def test_add_word_score_nonexistent_user(self):
         word_list = [("cat", "NOUN", 10)]
-        add_words_to_db(self.conn, word_list)
+        self.db_manager.add_words_to_db(self.conn, word_list)
         # Get the id of the word
         cur = self.conn.cursor()
         cur.execute("SELECT id FROM words WHERE word=? AND pos=?", ("cat", "NOUN"))
         word_id = cur.fetchone()[0]
         # Test adding a word score with a non-existent user
         with self.assertRaises(ValueDoesNotExistInDB):
-            add_word_score(self.conn, 999, word_id, 8)  # Attempting to add a score for a non-existent user should raise an error
+            self.db_manager.add_word_score(self.conn, 999, word_id, 8)  # Attempting to add a score for a non-existent user should raise an error
 
     def test_add_word_score_nonexistent_word_id(self):
         # Test adding a word score with a non-existent word_id
-        insert_user(self.conn, "test_user")
+        self.db_manager.insert_user(self.conn, "test_user")
         with self.assertRaises(ValueDoesNotExistInDB):
-            add_word_score(self.conn, 1, 999, 8)  # Attempting to add a score for a non-existent word_id should raise an error
+            self.db_manager.add_word_score(self.conn, 1, 999, 8)  # Attempting to add a score for a non-existent word_id should raise an error
 
     def test_add_word_score_incorrect_score(self):
         # Test adding a word score with an incorrect score
-        insert_user(self.conn, "test_user")
+        self.db_manager.insert_user(self.conn, "test_user")
         word_list = [("cat", "NOUN", 10)]
-        add_words_to_db(self.conn, word_list)
+        self.db_manager.add_words_to_db(self.conn, word_list)
         with self.assertRaises(ValueError):
-            add_word_score(self.conn, 1, 1, (MIN_SCORE - 1))  # Attempting to add a negative score should raise an error
+            self.db_manager.add_word_score(self.conn, 1, 1, (MIN_SCORE - 1))  # Attempting to add a negative score should raise an error
         with self.assertRaises(ValueError):
-            add_word_score(self.conn, 1, 1, (MAX_SCORE + 1))  # Attempting to add a score above the maximum should raise an error
+            self.db_manager.add_word_score(self.conn, 1, 1, (MAX_SCORE + 1))  # Attempting to add a score above the maximum should raise an error
 
 if __name__ == '__main__':
     unittest.main()
